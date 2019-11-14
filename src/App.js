c0fc59fe322469a5598f5c2f8177476f3f9d02de
andoreyrich <andoreyrich@gmail.com>
2019-11-14 23:19:13 +0100

    comment for yarn build

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SetCounterValue from "./SetCounterValue";
+import AppBox from "./AppBox";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+	state = {
+		counter: 0,
+		maxCounter: 5,
+		buttonCount: false,
+		buttonRes: false,
+		buttonSet: false,
+		startValue: 0,
+		maxValue: 5
+	};
+
+
+	onClickUp = () => {
+
+		if (this.state.counter < this.state.maxCounter) {
+			this.setState({counter: this.state.counter + 1}
+			)
+		}
+		if (this.state.counter === this.state.maxCounter - 1) {
+			this.setState({buttonCount: true})
+		}
+	};
+
+	onClickReset = () => {
+		this.setState({
+				counter: 0,
+				buttonCount: false
+			}
+		)
+	};
+
+	changeValue = () => {
+
+	};
+
+	onChangeStart = (event) => {
+		console.log(event.currentTarget)
+		this.setState({startValue: Number(event.currentTarget.value)},
+		() => {this.changeValue()}
+		)
+	};
+
+	onChangeMax = () => {
+
+	};
+
+
+	render() {
+		return (
+			<div className="App">
+				<AppBox state={this.state}
+						onClickUp={this.onClickUp}
+						onClickReset={this.onClickReset}/>
+				<SetCounterValue onChangeStart={this.onChangeStart}
+								 onChangeMax={this.onChangeMax}
+								 state={this.state}/>
+			</div>
+		)
+	}
 }
+
 
 export default App;
