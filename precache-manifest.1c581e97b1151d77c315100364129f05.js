0f1c230bd606c5928a25001faf02b4189ae286eb
andoreyrich <andoreyrich@gmail.com>
2019-11-14 23:18:26 +0100

    Updates

---
+++ precache-manifest.1c581e97b1151d77c315100364129f05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60d95c03291cc701f70a1ad53d6ba2dc",
+    "url": "/counter2/index.html"
+  },
+  {
+    "revision": "0a36c7eaded2b50c3686",
+    "url": "/counter2/static/css/main.075076bb.chunk.css"
+  },
+  {
+    "revision": "5f44fe968c44eba09ba3",
+    "url": "/counter2/static/js/2.687452e5.chunk.js"
+  },
+  {
+    "revision": "0a36c7eaded2b50c3686",
+    "url": "/counter2/static/js/main.f15833f7.chunk.js"
+  },
+  {
+    "revision": "3ff5c96c20e3ed5da074",
+    "url": "/counter2/static/js/runtime-main.6ee64885.js"
+  }
+]);
